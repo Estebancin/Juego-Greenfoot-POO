4844a0b4805b24295974bd5f290e34a941312f2b
Estebancin <esteban1110rp@gmail.com>
2023-07-31 09:38:26 -0600

    Corrección música 2
    Cambio de ruta

--- race-car/MyWorld.java
+++ race-car/MyWorld.java
@@ -44,8 +44,9 @@
             addRedCar();
             
             addRockets();
-            backgroundMusic1 = new GreenfootSound("./race-car/sounds/pista 2.mp3");
-            backgroundMusic2 = new GreenfootSound("./race-car/sounds/inicio.mp3");
+            backgroundMusic1 = new GreenfootSound("./pista 2.mp3");
+            backgroundMusic2 = new GreenfootSound("./inicio.mp3");
+
 
 
             backgroundMusic1.setVolume(70); // Ajustar el volumen (0-100)
